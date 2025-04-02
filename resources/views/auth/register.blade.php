1f5c85fa2c81df6b26b0025bc959c9194bb36cf8
codebaradev <bahnasep@gmail.com>
2025-04-02 18:48:00 +0800

    refactor: change the login and register view, change the profile logic and view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -30,7 +30,7 @@
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
-                         autocomplete="new-password" />
+                            autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -46,14 +46,17 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
+        <div class="flex mt-4">
+            <x-primary-button class="w-full">
                 {{ __('Register') }}
             </x-primary-button>
+        </div>
+
+        <div class="flex justify-center items-center mt-4">
+            <p class="text-slate-500">
+                Already have an account?
+                <a href="{{ route('login') }}" class="text-blue-500">Login</a>
+            </p>
         </div>
     </form>
 </x-guest-layout>
