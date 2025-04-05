d0a2b1825ef4fe09e0e307c7cad01d5f151889ec
codebaradev <bahnasep@gmail.com>
2025-04-06 05:20:49 +0800

    fix: home and dashboorad view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,80 +1,148 @@
 <x-app-layout>
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot> --}}
+    <section class="py-10">
+        <div class="container mx-auto px-10">
+            <div class="">
+                <div class="flex justify-between items-center mb-5">
+                    <h1 class="text-slate-500 text-xl font-bold">Shorten <span class="text-cyan-500">URLs</span></h1>
+                    <x-primary-button onclick="hideShowElm('shorten-new-url-container')" class="px-5 py-2">{{ __('Add') }}</x-primary-button>
+                </div>
+                {{-- Shorten Url --}}
+                <div id="shorten-new-url-container" class="{{ $errors->has('link') ? '' : 'hidden' }} fixed z-10 top-0 left-0 w-full h-full bg-slate-900/70" onclick="hideOnClickOutside(event, 'shorten-new-url-container')">
+                    <div class="p-10 w-1/2 border border-gray-500 rounded-lg bg-slate-800 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" onclick="event.stopPropagation()">
+                        <h1 class="text-xl text-slate-500 mb-2">Shorten <span class="text-cyan-500">URL</span></h1>
+                        <form method="post" action="/app/urls" class="flex flex-col gap-5">
+                            @csrf
 
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                            <div>
+                                <x-input-label class="mb-2" for="link" :value="__('Paste your URL here 👇')" />
+                                <x-text-input id="link" name="link" type="text" class=" block w-full" :value="old('link')" autofocus autocomplete="name" />
+                                <x-input-error class="mt-2" :messages="$errors->get('link')" />
+                            </div>
+
+                            <div class="">
+
+                                <x-secondary-button onclick="hideShowElm('shorten-new-url-container')" class="px-5">{{ __('Cancel') }}</x-secondary-button>
+                                <x-primary-button class="ml-3 px-5">{{ __('Shorten') }}</x-primary-button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="flex flex-col p-5 border min-h-[65vh] border-gray-500 rounded-lg bg-slate-700/10">
+
+                    @if (count($urls) == 0)
+                    <div class="flex justify-center items-center m-auto">
+                        <p class="text-xl text-slate-500 opacity-50">No <span class="text-cyan-500">URLs</span> have been shortened yet.</p>
+                    </div>
+                    @endif
+
+                    {{-- List of Urls --}}
+                    @foreach ($urls as $url)
+                        <div>
+                            <div onclick="hideShowElm('edit-url-{{ $url->short }}-container')" class="relative flex items-center cursor-pointer hover:bg-slate-800/50 p-5 mb-5 rounded-lg border border-gray-500 bg-slate-700/10">
+                                <div class="flex flex-col h-24 w-24 gap-2 justify-center items-center mr-2 bg-slate-800/70 rounded-lg p-2">
+                                    <span class="text-slate-300 text-2xl">{{ $url->clicks }}</span>
+                                    <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 7L5.5 5.5M15 7L16.5 5.5M5.5 16.5L7 15M11 5L11 3M5 11L3 11M17.1603 16.9887L21.0519 15.4659C21.4758 15.3001 21.4756 14.7003 21.0517 14.5346L11.6992 10.8799C11.2933 10.7213 10.8929 11.1217 11.0515 11.5276L14.7062 20.8801C14.8719 21.304 15.4717 21.3042 15.6375 20.8803L17.1603 16.9887Z" stroke="#06b6d4 " stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                                </div>
+                                <div class="flex-1">
+                                    @if (!empty($url->name))
+                                    <h3 class="text-slate-300 text-lg" > {{ $url->name }} </h3>
+                                    @endif
+                                    <div class="relative flex items-center ">
+                                        <h1 class="text-cyan-500 text-xl  hover:underline" onclick="event.stopPropagation(); copyUrl(event, 'copied{{ $url->short }}')">{{ env('APP_URL_2') }}/{{ $url->short }} </h1>
+                                        <span id="copied{{ $url->short }}" class="hidden text-green-500/75 pr-3 ml-3 ">Copied</span>
+                                    </div>
+                                    <h2 class="text-slate-500/50 text-md"> {{ $url->link }} </h2>
+                                </div>
+
+                                <div class="">
+                                    <form method="post" action="/app/urls/{{ $url->id }}" >
+                                        @method('DELETE')
+                                        @csrf
+
+                                        <button onclick="event.stopPropagation();" type="submit" class="text-red-500 hover:text-red-400 text-sm mt-2 p-4 flex justify-center items-center rounded-full border transition duration-100 border-red-700 hover:bg-red-700/10">
+                                            <svg width="20px" viewBox="0 0 1024 1024" fill="#ff0000" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#ff0000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M32 241.6c-11.2 0-20-8.8-20-20s8.8-20 20-20l940 1.6c11.2 0 20 8.8 20 20s-8.8 20-20 20L32 241.6zM186.4 282.4c0-11.2 8.8-20 20-20s20 8.8 20 20v688.8l585.6-6.4V289.6c0-11.2 8.8-20 20-20s20 8.8 20 20v716.8l-666.4 7.2V282.4z" fill=""></path><path d="M682.4 867.2c-11.2 0-20-8.8-20-20V372c0-11.2 8.8-20 20-20s20 8.8 20 20v475.2c0.8 11.2-8.8 20-20 20zM367.2 867.2c-11.2 0-20-8.8-20-20V372c0-11.2 8.8-20 20-20s20 8.8 20 20v475.2c0.8 11.2-8.8 20-20 20zM524.8 867.2c-11.2 0-20-8.8-20-20V372c0-11.2 8.8-20 20-20s20 8.8 20 20v475.2c0.8 11.2-8.8 20-20 20zM655.2 213.6v-48.8c0-17.6-14.4-32-32-32H418.4c-18.4 0-32 14.4-32 32.8V208h-40v-42.4c0-40 32.8-72.8 72.8-72.8H624c40 0 72.8 32.8 72.8 72.8v48.8h-41.6z" fill=""></path></g></svg>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <div id="edit-url-{{ $url->short }}-container" class="{{ $errors->any() ? '' : 'hidden' }} fixed z-10 top-0 left-0 w-full h-full bg-slate-900/70" onclick="hideOnClickOutside(event, 'edit-url-{{ $url->short }}-container')">
+                                <div class="p-10 w-1/2 border border-gray-500 rounded-lg bg-slate-800 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" onclick="event.stopPropagation()">
+                                    <h1 class="text-xl text-slate-500 mb-2">Edit Shorten <span class="text-cyan-500">URL</span></h1>
+                                    <form method="post" action="/app/urls/{{ $url->id }}" class="flex flex-col gap-5">
+                                        @method('PUT')
+                                        @csrf
+
+                                        <div>
+                                            <x-input-label for="name" :value="__('Name')" />
+                                            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $url->name)" autofocus autocomplete="name" />
+                                            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                                        </div>
+                                        <div>
+                                            <x-input-label for="short" :value="__('Shortened Url')" />
+                                            <div class="flex items-center">
+                                                <span class="text-slate-500">{{ env('APP_URL_2') }}</span>
+                                                <x-text-input id="short" name="short" type="text" class="flex-1 block w-full" :value="old('short', $url->short)" autofocus autocomplete="name" />
+                                            </div>
+                                            <x-input-error class="mt-2" :messages="$errors->get('short')" />
+                                        </div>
+                                        <div>
+                                            <x-input-label class="mb-2" for="link" :value="__('Paste your URL here 👇')" />
+                                            <x-text-input id="link" name="link" type="text" class=" block w-full" :value="old('link', $url->link)" autofocus autocomplete="name" />
+                                            <x-input-error class="mt-2" :messages="$errors->get('link')" />
+                                        </div>
+
+                                        <div class="">
+
+                                            <x-secondary-button onclick="hideShowElm('edit-url-{{ $url->short }}-container')" class="px-5">{{ __('Cancel') }}</x-secondary-button>
+                                            <x-primary-button class="ml-3 px-5">{{ __('Save') }}</x-primary-button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
-    </div> --}}
+    </section>
 
-    {{-- Create Url --}}
-    <div>
-        <form method="post" action="/app/urls">
-            @csrf
+    <script>
+        function copyUrl(event, copiedId) {
+            const urlElement = event.target;
+            const url = urlElement.innerHTML;
 
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
-                <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name')" autofocus autocomplete="name" />
-                <x-input-error class="mt-2" :messages="$errors->get('name')" />
-            </div>
-            <div>
-                <x-input-label for="link" :value="__('Url')" />
-                <x-text-input id="link" name="link" type="text" class="mt-1 block w-full" :value="old('link')" autofocus autocomplete="name" />
-                <x-input-error class="mt-2" :messages="$errors->get('link')" />
-            </div>
+            navigator.clipboard.writeText(url)
 
-            <x-secondary-button>{{ __('Cancel') }}</x-secondary-button>
-            <x-primary-button>{{ __('Add') }}</x-primary-button>
+            const copiedSpan = document.getElementById(copiedId);
+            if (copiedSpan) {
+                copiedSpan.classList.remove('hidden');
+                setTimeout(() => {
+                    copiedSpan.classList.add('hidden');
+                }, 1000);
+            }
+        }
 
-        </form>
-    </div>
+        function hideOnClickOutside(event, id) {
+            const element = document.getElementById(id);
+            if (element && event.target === element) {
+                element.classList.add('hidden');
+            }
+        }
 
-    {{-- List of Urls --}}
-    @foreach ($urls as $url)
-        <div>
-            <span class="text-white" > {{ $url->name }} </span>
-            <span class="text-white"> {{ $url->link }} </span>
-            <span class="text-white"> {{ $url->short }} </span>
-            <span class="text-white"> {{ $url->clicks }} </span>
+        function hideShowElm(id) {
+            const element = document.getElementById(id);
+            if (element) {
+                element.classList.toggle('hidden');
 
-            <form method="post" action="/app/urls/{{ $url->id }}">
-                @method('PUT')
-                @csrf
-
-                <div>
-                    <x-input-label for="name" :value="__('Name')" />
-                    <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $url->name)" autofocus autocomplete="name" />
-                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
-                </div>
-                <div>
-                    <x-input-label for="link" :value="__('Url')" />
-                    <x-text-input id="link" name="link" type="text" class="mt-1 block w-full" :value="old('link', $url->link)" autofocus autocomplete="name" />
-                    <x-input-error class="mt-2" :messages="$errors->get('link')" />
-                </div>
-                <div>
-                    <x-input-label for="short" :value="__('Shortened Url')" />
-                    <x-text-input id="short" name="short" type="text" class="mt-1 block w-full" :value="old('short', $url->short)" autofocus autocomplete="name" />
-                    <x-input-error class="mt-2" :messages="$errors->get('short')" />
-                </div>
-
-                <x-secondary-button>{{ __('Cancel') }}</x-secondary-button>
-                <x-primary-button>{{ __('Save') }}</x-primary-button>
-            </form>
-
-            <form method="post" action="/app/urls/{{ $url->id }}" >
-                @method('DELETE')
-                @csrf
-
-                <x-secondary-button type="submit">{{ __('Delete') }}</x-secondary-button>
-            </form>
-        </div>
-    @endforeach
+                if (!element.classList.contains('hidden')) {
+                    const urlInput = document.querySelector(`#${id} input[type="text"]`);
+                    if (urlInput) {
+                        urlInput.focus();
+                    }
+                }
+            }
+        }
+    </script>
 </x-app-layout>
--- resources/views/url/shorten.blade.php
+++ resources/views/url/shorten.blade.php
@@ -3,15 +3,17 @@
         <div class="container mx-auto px-16">
             <div class="border border-gray-500 rounded-lg bg-slate-700/10 ">
                 <div class="flex flex-col justify-center text-center p-40">
-                    <div class="flex flex-col mb-7">
+                    <div class="flex flex-col mb-7 ">
                         <label class="text-lg mb-3 font-bold text-slate-500" for="link">{{ __('Shorten URL Success ✅') }}</label>
                         <div class="flex">
                             <span class="border border-slate-500 flex px-2 items-center flex-1 text-left text-blue-500">
                                 <a id="shorten-url" class="" href="http://127.0.0.1:8000/{{ $url->short }}">http://127.0.0.1:8000/{{ $url->short }}</a>
                             </span>
-                            <x-primary-button type="button" onclick="copyUrl('shorten-url', 'copied')"  class="mx-auto px-5 py-3 rounded-l-none">{{ __('Copy') }}</x-primary-button>
+                            <div class="relative flex flex-col justify-center">
+                                <x-primary-button type="button" onclick="copyUrl('shorten-url', 'copied')"  class="mx-auto px-5 py-3 rounded-l-none">{{ __('Copy') }}</x-primary-button>
+                                <span id="copied" class="hidden absolute top-full text-right text-green-500/75 pr-3">Copied</span>
+                            </div>
                         </div>
-                        <span id="copied" class="hidden text-right text-green-500 pr-3">Copied</span>
                         <a class="mt-5 text-slate-500 transition duration-100 hover:text-blue-500" href="/">Shorten another URL?</a>
                     </div>
                 </div>
@@ -27,7 +29,12 @@
 
             navigator.clipboard.writeText(url)
 
-            copiedElement.style.display = 'block';
+            if (copiedElement) {
+                copiedElement.classList.remove('hidden');
+                setTimeout(() => {
+                    copiedElement.classList.add('hidden');
+                }, 1000);
+            }
         }
     </script>
 </x-app-layout>
