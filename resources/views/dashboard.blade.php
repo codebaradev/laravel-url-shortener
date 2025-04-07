b15e94d9fad7d2de9961e6fb02ded5ffe9eab730
codebaradev <bahnasep@gmail.com>
2025-04-07 20:09:09 +0800

    fix: search bar value

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
                             <button class="p-1 rounded-full hover:bg-slate-500/10">
                                 <svg width="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#64748b "><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#64748b " stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                             </button>
-                            <input id="search" name="search" type="text" placeholder="Search" class="w-full p-0 border-none outline-none bg-transparent text-slate-300 focus:outline-none focus:ring-0 focus:border-transparent" :value="old('search')" />
+                            <input id="search" name="search" type="text" placeholder="Search" value="{{ request('search') }}" class="w-full p-0 border-none outline-none bg-transparent text-slate-300 focus:outline-none focus:ring-0 focus:border-transparent" />
                         </div>
                     </form>
                     <x-primary-button onclick="hideShowElm('shorten-new-url-container')" class="px-5 py-2">{{ __('Add') }}</x-primary-button>
